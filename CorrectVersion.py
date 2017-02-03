4ed609d5fe259a7ea7d823aea60ad31e89ca3ea4
arashDeylami <arash_deylamsalehi@student.uml.edu>
2017-02-02 20:46:24 -0500

    Update CorrectVersion.py
    This is a correct version based on the recursive method.

--- CorrectVersion.py
+++ CorrectVersion.py
@@ -1,19 +1,15 @@
 # Permutation
 
 def permutation(newString):
-if len(newString) == 1:
-        return newString
-    else:
-        result = []
-        for i in newString:
-            eleminatedString = newString.replace(i,'',1)
-            for j in permutation(eleminatedString):
-                result.append(i+j)
-                
-    return result
+        if len(newString) == 1:
+                return newString
+        else:
+                result = []
+                for i in newString:
+                        eleminatedString = newString.replace(i,'',1)
+                        for j in permutation(eleminatedString):
+                                result.append(i+j)
+        return result
     
     
-print(permutation('arash')) 
-        
-  
-    
+
